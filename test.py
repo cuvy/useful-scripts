da53f764dd63a7596b18dc92f36686124ff68f4e
Phil Cole <14947837+cuvy@users.noreply.github.com>
2018-05-16 10:01:11 +1000

    test file

---
+++ test.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import socket
+
+#f = open("/etc/passwd", 'r')
+#sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+#payload = f.read()
+#sock.sendto(bytes(payload), ("127.0.0.1", 5555))
+
+# an explanation here f = open("/etc/passwd", 'r')
+# some morei sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+# asdfadsf payload = f.read()
+
+# asdfsadf sock.sendto(bytes(payload), (127.0.0.1, 5555))
+#uh-oh, don't do thisprint("this is a commented out print statement")
+print("this is a valid print statement")
